778e03b84aa466fae7c1ef57228c2391615afcc8
mishalisovskiy <mishalisovskiy@gmail.com>
2019-12-05 16:46:21 +0200

    added templates to views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 from .models import Question
 
@@ -7,12 +9,15 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at the question no. {}".format(question_id))
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': q})
 
 
 def results(request, question_id):
