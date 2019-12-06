b4847e2c63dd46a3c1a3118c25a621aec68c5eef
Mykhailo <mishalisovskiy@gmail.com>
2019-12-06 11:48:15 +0200

    added polls vote logic

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -21,9 +22,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "you're looking at the results of question no {}"
-    return HttpResponse(response.format(question_id))
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': q})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're looking at the question no. {}".format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            'polls/detail.hrml',
+            {
+                'question': question,
+                'error_message': 'You did not select a valid choice',
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
